cd2f51f4c159320ad9a33b2cea5eb960304ef5ca
Ryan <dalerr@niddk.nih.gov>
2010-06-01 17:23:55 -0400

    test

--- makeConstitutiveExons.py
+++ makeConstitutiveExons.py
@@ -8,19 +8,21 @@ import GFFutils
 op = optparse.OptionParser()
 op.add_option('--dbfn', help='GFF (or GTF) database as created by GFFutils')
 op.add_option('-o', help='Output file to write exons to')
-op.add_option('--level', help='Specify --level=exon or --level=gene to determine which ID '
+op.add_option('--level', help='Specify --level=exons or --level=genes to determine which ID '
                               'will be inserted in the gene_id attribute of the output GFF file.')
 op.add_option('--type', default='GFF', help='Use --type=GTF for GTF files.')
 options,args = op.parse_args()
 
 if options.type == 'GFF':
     G = GFFutils.GFFDB(options.dbfn)
+    exon_feature = 'exon'
 if options.type == 'GTF':
     G = GFFutils.GTFDB(options.dbfn)
+    exon_feature = 'CDS'
 
 if __name__ == "__main__":
 
-    fout = open(options.o,'w')
+    fout = open('const.exons.named-for-genes.bed','w')
 
     # make a dictionary of each gene and the number of isoforms it has
     genes = {}
@@ -42,11 +44,7 @@ if __name__ == "__main__":
             gene_isoforms = genes[exon_genes[exon]]
             if exon_isoforms == gene_isoforms:
                 e = G[exon]
-                if options.level == 'exon':
-                    gene_id = exon
-                if options.level == 'gene':
-                    gene_id = exon_genes[exon]
-                e.add_attribute('gene_id',gene_id)
+                e.add_attribute('gene_id',exon_genes[exon])
                 fout.write(e.tostring())
         except KeyError:
             pass
